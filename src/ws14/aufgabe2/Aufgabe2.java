e820c5f8331455278a79e52cc096c150fdc3acc4
Hassel Stefan <shutefansan@gmail.com>
2016-07-15 14:07:04 +0200

    Added Sortieralgorithmus aus der Vorlesung

--- src/ws14/aufgabe2/Aufgabe2.java
+++ src/ws14/aufgabe2/Aufgabe2.java
@@ -1,7 +1,8 @@
 package ws14.aufgabe2;
 
 public class Aufgabe2 {
-
+	
+	// Lösung mit Hilfe des BubbleSort
 	public static int[] sortiereAbsteigend(int[] array) {
 		int temp;
 		for (int i = 0; i < array.length; i++) {
@@ -15,6 +16,32 @@ public class Aufgabe2 {
 		}
 		return array;
 	}
+	
+	// Lösung nach dem Algorithmus aus der Vorlesung
+	public static int[] sortiereAbsteigend2(int[] array){
+		int temp;
+		int gesamtAnzahl = array.length;
+		int indexHoechsterZahl;
+		
+		// j gibt immer die Stelle im Array an, ab der der unsortierte Teil beginnt
+		for (int j = 0;j < gesamtAnzahl-1; j++ ){
+			indexHoechsterZahl = j;
+			
+			// suche im verbleibenden Array die größte Zahl 
+			for(int i = j+1; i< gesamtAnzahl; i++){
+				if (array[i]>array[indexHoechsterZahl]){
+					indexHoechsterZahl = i;
+				}
+			}
+			
+			// Die gefundene höchste Zahl mit der Zahl ander Stelle j tauschen
+			temp = array[j];
+			array[j] = array[indexHoechsterZahl];
+			array[indexHoechsterZahl]=temp;
+		}
+		return array;
+	}
+
 
 	public static void main(String[] args) {
 
