d4359604b5b0648cfeb14387bcbe0a3f5ac0c1d4
Henry Vu <HenrytheVU@gmail.com>
2016-07-17 18:11:24 +0200

    WS13 Aufgabge 5 added

---
+++ src/ws13/aufgabe5/Liste.java
@@ -0,0 +1,43 @@
+package ws13.aufgabe5;
+
+public class Liste {
+
+	private Knoten start;
+
+	public Liste(Knoten start) {
+		this.start = start;
+	}
+
+	public Knoten getStart() {
+		return start;
+	}
+
+	public void setStart(Knoten start) {
+		this.start = start;
+	}
+
+	public String[] toArray() {
+		int length = 0;
+		Knoten temp = start;
+		while (temp != null) {
+			length++;
+			temp = temp.getNext();
+		}
+		String[] result = new String[length];
+		temp = start;
+		for (int i = 0; i < result.length; i++) {
+			result[i] = temp.getName();
+			temp = temp.getNext();
+		}
+		return result;
+	}
+
+	// main-Methode zum Testen
+	public static void main(String[] args) {
+		Liste list = new Liste(new Knoten("K1", new Knoten("K2", new Knoten("K3", null))));
+		for (String s : list.toArray()) {
+			System.out.println(s);
+		}
+	}
+
+}
